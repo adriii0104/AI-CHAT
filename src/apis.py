64f1ddd2bf9407c4d0c3d053302d094986bdb45d
adriii0104 <adriel.capellan1@icloud.com>
2023-05-26 15:41:41 -0400

    Agregue modulos de sentimientos

---
+++ src/apis.py
@@ -0,0 +1,14 @@
+import requests
+
+# Función para obtener información de Wikipedia
+def obtener_informacion_wikipedia(palabra_clave):
+    url = f"https://es.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&titles={palabra_clave}&format=json"
+    response = requests.get(url).json()
+
+    if 'query' in response and 'pages' in response['query']:
+        pages = response['query']['pages']
+        for page in pages.values():
+            if 'extract' in page:
+                return page['extract']
+
+    return None
