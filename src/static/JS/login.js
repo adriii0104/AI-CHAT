3d945d1756320c123356d8b39aa28332b8bbd0f3
adriii0104 <adriel.capellan1@icloud.com>
2023-05-28 18:21:12 -0400

    agregue el microfono y reconocimiento de voz

---
+++ src/static/JS/login.js
@@ -0,0 +1,5 @@
+if (error !== '') {
+    document.getElementById('error').innerHTML = "Usuario o contraseña incorrectos";
+    document.getElementById('usuario').style.borderBottom ="2px solid #9b0b0b;"
+    document.getElementById('contraseña').style.borderBottom ="2px solid #9b0b0b;"
+}
