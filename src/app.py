9a98e6badfcfa6b6ec45ae62ea3f71da77d9e3c5
adriii0104 <adriel.capellan1@icloud.com>
2023-05-28 18:27:04 -0400

    agregue respuestas a comandos de voz

--- src/app.py
+++ src/app.py
@@ -1,14 +1,24 @@
 import json
 import nltk
 import spacy
-import pickle
-from flask import Flask, render_template, request
+import pyttsx3
+from flask import Flask, render_template, request, url_for, redirect, session
 import openai
 from sklearn.feature_extraction.text import CountVectorizer
-import pyttsx3
-from config import mi_api
+from flask_mysqldb import MySQL
+from config import mi_api, SECRET
+import speech_recognition as sr
+
 
 app = Flask(__name__)
+
+# Almacenaremos la conexión
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'ia'
+app.config['SECRET_KEY'] = SECRET
+mysql = MySQL(app)
 
 # Configurar la clave de la API de OpenAI
 openai.api_key = mi_api
@@ -18,11 +28,11 @@ nlp = spacy.load('es_core_news_sm')
 
 # Archivo JSON con información contextual
 ARCHIVO_CONTEXTUAL = 'contexto.json'
-ARCHIVO_CAMBIOS = 'cambios.json'
+ARCHIVO_CONVERSACIONES = 'conversaciones.json'
 
 # Crear archivos necesarios si no existen
 def crear_archivos():
-    archivos = [ARCHIVO_CONTEXTUAL, ARCHIVO_CAMBIOS]
+    archivos = [ARCHIVO_CONTEXTUAL, ARCHIVO_CONVERSACIONES]
 
     for archivo in archivos:
         try:
@@ -47,67 +57,69 @@ def guardar_contexto(contexto):
     with open(ARCHIVO_CONTEXTUAL, 'w') as file:
         json.dump(contexto, file)
 
-# Cargar datos previos desde el archivo
-def cargar_datos():
+# Cargar conversaciones desde el archivo JSON
+def cargar_conversaciones():
     try:
-        with open('conversaciones.json', 'r') as file:
+        with open(ARCHIVO_CONVERSACIONES, 'r') as file:
             conversaciones = json.load(file)
     except FileNotFoundError:
-        conversaciones = []
+        conversaciones = {}
 
-    try:
-        with open(ARCHIVO_CAMBIOS, 'r') as file:
-            cambios = json.load(file)
-    except FileNotFoundError:
-        cambios = {}
+    return conversaciones
 
-    return conversaciones, cambios
-
-# Guardar datos en los archivos correspondientes
-def guardar_datos(conversaciones, cambios):
-    with open('conversaciones.json', 'w') as file:
+# Guardar conversaciones en el archivo JSON
+def guardar_conversaciones(conversaciones):
+    with open(ARCHIVO_CONVERSACIONES, 'w') as file:
         json.dump(conversaciones, file)
 
-    with open(ARCHIVO_CAMBIOS, 'w') as file:
-        json.dump(cambios, file)
-
 # Obtener respuesta utilizando la API de OpenAI
-def obtener_respuesta_gpt3(pregunta):
-    global conversaciones, cambios
-
+def obtener_respuesta_gpt3(pregunta, conversaciones):
     # Cargar información contextual
     contexto = cargar_contexto()
     contexto['pregunta'] = pregunta
     guardar_contexto(contexto)
 
-    conversacion_actual = conversaciones + [(pregunta, '')]
+    conversaciones_list = [(key, value) for key, value in conversaciones.items()]
+    conversacion_actual = conversaciones_list + [(pregunta, '')]
     entrada = '\n'.join(f'Usuario: {user}\nBot: {bot}' for user, bot in conversacion_actual)
-    respuesta = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=entrada,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-    nueva_respuesta = respuesta.choices[0].text.strip()
+    tokens_entrada = nltk.word_tokenize(entrada)
+
+    # Dividir la entrada en partes más pequeñas para reducir los gastos de tokens
+    partes_entrada = []
+    inicio = 0
+    while inicio < len(tokens_entrada):
+        fin = min(inicio + 4096, len(tokens_entrada))
+        partes_entrada.append(' '.join(tokens_entrada[inicio:fin]))
+        inicio = fin
+
+    # Obtener respuestas para cada parte de la entrada
+    respuestas = []
+    for parte_entrada in partes_entrada:
+        respuesta = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=parte_entrada,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.7
+        )
+        nueva_respuesta = respuesta.choices[0].text.strip()
+        respuestas.append(nueva_respuesta)
+
+    # Combinar todas las respuestas
+    nueva_respuesta = ' '.join(respuestas)
 
     # Agregar la nueva respuesta al cerebro de conversaciones sin consumir todos los tokens
     if len(conversaciones) > 0:
-        tokens_cerebro = sum(len(nltk.word_tokenize(bot)) for _, bot in conversaciones)
+        tokens_cerebro = sum(len(nltk.word_tokenize(bot)) for _, bot in conversaciones.items())
         tokens_nueva_respuesta = len(nltk.word_tokenize(nueva_respuesta))
         if tokens_cerebro + tokens_nueva_respuesta <= 4096:  # Límite máximo de tokens
-            conversaciones.append((pregunta, nueva_respuesta))
-
-    # Guardar cambios en el archivo
-    guardar_datos(conversaciones, cambios)
+            conversaciones[pregunta] = nueva_respuesta
 
     return nueva_respuesta
 
 # Realizar aprendizaje activo para mejorar las respuestas
-def aprendizaje_activo(pregunta, respuesta):
-    global cambios
-
+def aprendizaje_activo(pregunta, respuesta, cambios):
     if pregunta.strip() == '':
         return
 
@@ -126,7 +138,7 @@ def aprendizaje_activo(pregunta, respuesta):
     cambios[pregunta.lower()] = respuesta
 
     # Guardar cambios en el archivo
-    guardar_datos(conversaciones, cambios)
+    guardar_conversaciones(cambios)
 
 # Convertir texto a voz utilizando pyttsx3
 def convertir_texto_a_voz(texto):
@@ -134,17 +146,108 @@ def convertir_texto_a_voz(texto):
     engine.save_to_file(texto, 'respuesta.wav')
     engine.runAndWait()
 
+# Estos son los corredores, ej. login, register, chat, etc.
+
+@app.before_request
+def before_request():
+    allowed_routes = ['login', 'register']
+    if 'usuario' in session and request.endpoint in allowed_routes:
+        return redirect(url_for('index'))
+    elif 'usuario' not in session and request.endpoint not in allowed_routes and not request.path.startswith('/static'):
+        return redirect(url_for('login'))
+
+# Registro
+@app.route('/auth/register')
+def register():
+    return render_template('auth/register.html')
+
+# Login
+@app.route('/auth', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        contraseña = request.form['contraseña']
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM usuarios WHERE usuario = %s ", (usuario,))
+        user = cur.fetchone()
+        cur.close()
+        if user is not None:
+            contraseñaalma = user[1]
+            if contraseña == contraseñaalma:
+                session['usuario'] = user[0]
+                return redirect(url_for('index'))
+            else:
+                return render_template('auth/login.html', error='Usuario o contraseña incorrecto')
+        else:
+            return render_template('auth/login.html', error='Usuario o contraseña incorrecto')
+    else:
+        return render_template('auth/login.html')
+
+# Cierre de sesión
+@app.route('/salir')
+def salir():
+    session.clear()
+    return redirect(url_for('login'))
+
+# Ruta principal, chat
 @app.route('/')
 def index():
     return render_template('chat.html')
 
+
+
+
+
+
+
+@app.route('/speech-to-text', methods=['POST'])
+def speech_to_text():
+    # Obtener el archivo de audio enviado por el cliente
+    audio_file = request.files['audio']
+
+    # Crear un reconocedor de voz
+    recognizer = sr.Recognizer()
+
+    try:
+        # Leer el archivo de audio utilizando SpeechRecognition
+        with sr.AudioFile(audio_file) as source:
+            audio_data = recognizer.record(source)
+
+        # Realizar el reconocimiento de voz
+        text = recognizer.recognize_google(audio_data, language='es-ES')
+
+        # Devolver el texto reconocido como respuesta
+        return text
+    except Exception as e:
+        # Manejar cualquier error que ocurra durante el reconocimiento de voz
+        return str(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Ruta para las respuestas
 @app.route('/get_response', methods=['POST'])
 def get_response():
     mensaje = request.form['user_message']
-    response = obtener_respuesta_gpt3(mensaje)
+    conversaciones = cargar_conversaciones()
+    response = obtener_respuesta_gpt3(mensaje, conversaciones)
 
     # Realizar aprendizaje activo
-    aprendizaje_activo(mensaje, response)
+    aprendizaje_activo(mensaje, response, conversaciones)
+
+    # Guardar conversaciones actualizadas
+    guardar_conversaciones(conversaciones)
 
     # Convertir la respuesta de texto a voz
     convertir_texto_a_voz(response)
@@ -155,7 +258,7 @@ if __name__ == '__main__':
     crear_archivos()
 
     # Cargar datos previos
-    conversaciones, cambios = cargar_datos()
+    cambios = cargar_conversaciones()
 
     # Iniciar la aplicación Flask
     app.run(debug=True)
