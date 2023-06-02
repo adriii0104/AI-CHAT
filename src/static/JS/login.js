8788351274efbee0a15858dc30d648ae2518f494
adriii0104 <adriel.capellan1@icloud.com>
2023-06-02 18:03:10 -0400

    debugg login.js

--- src/static/JS/login.js
+++ src/static/JS/login.js
@@ -1,14 +1,15 @@
 function togglePasswordVisibility() {
-  var contraseñaInput = document.getElementById("contraseña");
-  var iconoOjo = document.querySelector(".fi-sr-eye");
-
-  if (contraseñaInput.type == "password") {
-    contraseñaInput.type = "text";
-    iconoOjo.classList.remove("fi-sr-eye");
-    iconoOjo.classList.add("fi-sr-eye-crossed");
-  } else {
-    contraseñaInput.type = "password";
-    iconoOjo.classList.remove("fi-sr-eye-crossed");
-    iconoOjo.classList.add("fi-sr-eye");
+    var contraseñaInput = document.getElementById("contraseña");
+    var iconoOjos = document.querySelectorAll(".fi-sr-eye");
+  
+    if (contraseñaInput.type === "password") {
+      contraseñaInput.type = "text";
+      iconoOjos[0].classList.remove("fi-sr-eye");
+      iconoOjos[0].classList.add("fi-sr-eye-crossed");
+    } else {
+      contraseñaInput.type = "password";
+      iconoOjos[0].classList.remove("fi-sr-eye-crossed");
+      iconoOjos[0].classList.add("fi-sr-eye");
+    }
   }
-}
+  
