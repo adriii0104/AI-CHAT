652b18736ce78df8261afb86e4c2ba152f224f43
adriii0104 <adriel.capellan1@icloud.com>
2023-06-09 19:17:12 -0400

    User Api request, ADDED

--- src/app.py
+++ src/app.py
@@ -17,7 +17,9 @@ from PyPDF2 import PdfReader, PdfWriter
 from utils.extraccion import correos
 from docx import Document
 import os
-
+import traceback
+import hashlib
+import uuid
 
 app = Flask(__name__)
 
@@ -35,21 +37,31 @@ openai.api_key = mi_api
 # Cargar modelo de SpaCy en español
 nlp = spacy.load('es_core_news_sm')
 
+
+
 # Archivo JSON con información contextual
 ARCHIVO_CONTEXTUAL = 'contexto.json'
-ARCHIVO_CONVERSACIONES = 'conversaciones.json'
 
 # Crear archivos necesarios si no existen
-def crear_archivos():
-    archivos = [ARCHIVO_CONTEXTUAL, ARCHIVO_CONVERSACIONES]
-
+def crear_archivos(email_usuario):
+    carpeta_usuario = "archivos_usuario"
+    archivo_conversaciones = f"{email_usuario}.json"
+    archivos = [ARCHIVO_CONTEXTUAL, archivo_conversaciones]
+    
+    # Crea la carpeta si no existe
+    if not os.path.exists(carpeta_usuario):
+        os.makedirs(carpeta_usuario)
+    
     for archivo in archivos:
+        ruta_archivo = os.path.join(carpeta_usuario, archivo)
         try:
-            with open(archivo, 'r') as f:
+            with open(ruta_archivo, 'r') as f:
                 pass
         except FileNotFoundError:
-            with open(archivo, 'w') as f:
+            with open(ruta_archivo, 'w') as f:
                 json.dump({}, f)
+
+
 
 # Cargar información contextual desde el archivo JSON
 def cargar_contexto():
@@ -61,48 +73,72 @@ def cargar_contexto():
 
     return contexto
 
+
+
 # Guardar información contextual en el archivo JSON
 def guardar_contexto(contexto):
     with open(ARCHIVO_CONTEXTUAL, 'w') as file:
+        print(contexto)
         json.dump(contexto, file)
 
 # Cargar conversaciones desde el archivo JSON
-def cargar_conversaciones():
-    try:
-        with open(ARCHIVO_CONVERSACIONES, 'r') as file:
+def cargar_conversaciones(email_usuario):
+    if 'usuario' in session:
+        archivo_conversaciones = f"{email_usuario}.json"
+        with open(archivo_conversaciones, 'r') as file:
             conversaciones = json.load(file)
-    except FileNotFoundError:
-        conversaciones = {}
+            return conversaciones
+    else:
+        # Manejar el caso cuando no se encuentra el usuario en la sesión
+        return []
 
-    return conversaciones
+
 
 # Guardar conversaciones en el archivo JSON
-def guardar_conversaciones(conversaciones):
-    with open(ARCHIVO_CONVERSACIONES, 'w') as file:
-        json.dump(conversaciones, file)
+def guardar_conversaciones(email_usuario, conversaciones):
+    if 'usuario' in session:
+        archivo_conversaciones = f"{email_usuario}.json"
+        try:
+            with open(archivo_conversaciones, 'r') as file:
+                conversaciones_previas = json.load(file)
+        except FileNotFoundError:
+            conversaciones_previas = []  # Si el archivo no existe, crear una nueva lista de conversaciones
+
+        if isinstance(conversaciones, list):
+            conversaciones_previas.append(conversaciones)  # Agregar las nuevas conversaciones a la lista previa
+
+        try:
+            with open(archivo_conversaciones, 'w') as file:
+                json.dump(conversaciones_previas, file)
+        except Exception as e:
+            traceback.print_exc()  # Imprimir la traza de error
+
+
 
 # Obtener respuesta utilizando la API de OpenAI
 def obtener_respuesta_gpt3(pregunta, conversaciones):
-    # Cargar información contextual
-    contexto = cargar_contexto()
-    contexto['pregunta'] = pregunta
-    guardar_contexto(contexto)
+    if 'usuario' in session:
+        contexto = cargar_contexto()
+        contexto['pregunta'] = pregunta
+        guardar_contexto(contexto)
+        
 
-    conversaciones_list = [(key, value) for key, value in conversaciones.items()]
-    conversacion_actual = conversaciones_list + [(pregunta, '')]
-    entrada = '\n'.join(f'Usuario: {user}\nBot: {bot}' for user, bot in conversacion_actual)
-    tokens_entrada = nltk.word_tokenize(entrada)
 
-    # Dividir la entrada en partes más pequeñas para reducir los gastos de tokens
-    partes_entrada = []
-    inicio = 0
-    while inicio < len(tokens_entrada):
-        fin = min(inicio + 4096, len(tokens_entrada))
-        partes_entrada.append(' '.join(tokens_entrada[inicio:fin]))
-        inicio = fin
+        conversaciones_list = [(key, value) for key, value in conversaciones.items()]
+        conversacion_actual = conversaciones_list + [(pregunta, '')]
+        entrada = '\n'.join(f'Usuario: {user}\nBot: {bot}' for user, bot in conversacion_actual)
+        tokens_entrada = nltk.word_tokenize(entrada)
 
-    # Obtener respuestas para cada parte de la entrada
-    respuestas = []
+        # Dividir la entrada en partes más pequeñas para reducir los gastos de tokens
+        partes_entrada = []
+        inicio = 0
+        while inicio < len(tokens_entrada):
+                fin = min(inicio + 4096, len(tokens_entrada))
+                partes_entrada.append(' '.join(tokens_entrada[inicio:fin]))
+                inicio = fin
+
+        # Obtener respuestas para cada parte de la entrada
+        respuestas = []
     for parte_entrada in partes_entrada:
         respuesta = openai.Completion.create(
             engine='text-davinci-003',
@@ -117,37 +153,12 @@ def obtener_respuesta_gpt3(pregunta, conversaciones):
 
     # Combinar todas las respuestas
     nueva_respuesta = ' '.join(respuestas)
-
     # Agregar la nueva respuesta al cerebro de conversaciones sin consumir todos los tokens
-    if len(conversaciones) > 0:
-        tokens_cerebro = sum(len(nltk.word_tokenize(bot)) for _, bot in conversaciones.items())
-        tokens_nueva_respuesta = len(nltk.word_tokenize(nueva_respuesta))
-        if tokens_cerebro + tokens_nueva_respuesta <= 4096:  # Límite máximo de tokens
-            conversaciones[pregunta] = nueva_respuesta
+
 
     return nueva_respuesta
 
-# Realizar aprendizaje activo para mejorar las respuestas
-def aprendizaje_activo(pregunta, respuesta, cambios):
-    if pregunta.strip() == '':
-        return
 
-    if respuesta.strip() == '':
-        return
-
-    # Tokenizar la pregunta
-    tokenizer = CountVectorizer().build_tokenizer()
-    tokens = tokenizer(pregunta.lower())
-
-    # Realizar aprendizaje activo solo si la pregunta tiene más de 3 palabras
-    if len(tokens) <= 3:
-        return
-
-    # Agregar la pregunta y su respuesta al diccionario de cambios
-    cambios[pregunta.lower()] = respuesta
-
-    # Guardar cambios en el archivo
-    guardar_conversaciones(cambios)
 
 
 
@@ -227,43 +238,7 @@ def download_file():
     return send_file(filename, as_attachment=True)
 
 
-
-
-
-
-
-
-
-
-
-
-
 # Estos son los corredores, ej. login, register, chat, etc.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -291,13 +266,17 @@ def before_request():
 @app.route('/auth/register', methods=["POST", "GET"])
 def register():
     if request.method == "POST":
+            id_usuario = str(uuid.uuid4())
             nombre = request.form.get('nombre')
             email = request.form.get('email')
             password = request.form.get('contraseña')
+            hashpassword = hashlib.sha256()
+            hashpassword.update(password.encode('utf8'))
+            hash_value = hashpassword.hexdigest()
+            Hashapi = request.form.get('openai')
             code = 0
             reason = "NO"
             numeros = ''.join(random.choices(string.digits, k=4))
-            codigo = "BLID" + numeros
             cur1 = mysql.connection.cursor()
             cur1.execute("SELECT usuario from usuarios Where usuario = %s", (email, ))
             user = cur1.fetchone()
@@ -305,16 +284,17 @@ def register():
 
                 cur1 = mysql.connection.cursor()
                 cur1.execute("INSERT INTO verificacion VALUES (%s, %s, %s, %s)",
-                             (codigo, reason, numeros, datetime.now()))
+                             (id_usuario, reason, numeros, datetime.now()))
                 
                 cur1.close()
 
                 cur = mysql.connection.cursor()
-                cur.execute("INSERT INTO usuarios VALUES (%s, %s, %s, %s, %s)",
-                             (code, codigo, nombre, email, password))
+                cur.execute("INSERT INTO usuarios VALUES (%s, %s, %s, %s, %s, %s)",
+                             (code, id_usuario, nombre, email, hash_value, Hashapi))
                 
                 mysql.connection.commit()
 
+                flash("Felicidades, ya casi eres parte de Genuine. Inicia Sesión por seguridad.")
                 return redirect(url_for('login'))
             else:
                 return render_template('auth/register.html', error=error)
@@ -330,6 +310,9 @@ def login():
     if request.method == 'POST':
         usuario = request.form['email']
         contraseña = request.form['contraseña']
+        contraseñahasheada = hashlib.sha256()
+        contraseñahasheada.update(contraseña.encode('utf8'))
+        hash_ingresada = contraseñahasheada.hexdigest()
 
         cur = mysql.connection.cursor()
         cur.execute("SELECT * FROM usuarios WHERE usuario = %s ", (usuario,))
@@ -338,11 +321,13 @@ def login():
 
         if user is not None:
             contraseñaalma = user[4]
-            if contraseña == contraseñaalma:
+            if hash_ingresada == contraseñaalma:  # Comparar el hash ingresado con el hash almacenado
                 session['id'] = user[0]
                 session['idgenuine'] = user[1]
                 session['nombre'] = user[2]
                 session['usuario'] = user[3]
+                email_usuario = session['usuario']
+                crear_archivos(email_usuario)
 
                 cur1 = mysql.connection.cursor()
                 cur1.execute("SELECT * FROM verificacion WHERE idblid = %s ", (session['idgenuine'],))
@@ -371,6 +356,7 @@ def login():
 
 
 
+
 @app.route('/auth/verificacion', methods=["GET", "POST"])
 def verificacion():
     if request.method == 'POST':
@@ -387,6 +373,14 @@ def verificacion():
                 mysql.connection.commit()
                 cur.close()
                 session['verificado'] = "SI"
+
+                # Crear el archivo JSON para el usuario
+                email_usuario = session['idgenuine']
+                archivo_usuario = f"{email_usuario}.json"
+                if not os.path.exists(archivo_usuario):
+                    with open(archivo_usuario, 'w') as f:
+                        json.dump({}, f)
+
                 return redirect(url_for('index'))
             else:
                 return render_template('auth/verificacion.html', error='Código incorrecto')
@@ -401,12 +395,12 @@ def verificacion():
             destinatario = session.get('usuario')
             asunto = 'Tu código de verificación Genuine'
             contenido = 'Aquí está tu código de verificación de Genuine: {}'
-            
+
             # Verificar si ya se envió un código de verificación antes
             if 'codigo_enviado' not in session:
                 enviar_correo(destinatario, asunto, contenido.format(code[2]))
                 session['codigo_enviado'] = True  # Establecer la bandera de código enviado
-            
+
         return render_template('auth/verificacion.html')
 
     return redirect(url_for('index'))
@@ -455,13 +449,17 @@ def resend():
 # Cierre de sesión
 @app.route('/salir')
 def salir():
+    email_usuario = session.get('idgenuine')
+    conversaciones = cargar_conversaciones(email_usuario)
+    guardar_conversaciones(email_usuario, conversaciones)
     session.clear()
     return redirect(url_for('home'))
-
 # Ruta principal, chat
 @app.route('/')
 def index():
-    return render_template('chat.html')
+    email_usuario = session.get('idgenuine')
+    conversaciones = cargar_conversaciones(email_usuario)
+    return render_template('chat.html', conversaciones=conversaciones)
 
 
 @app.route('/home')
@@ -531,6 +529,7 @@ def speech_to_text():
 @app.route('/get_response', methods=['POST'])
 def get_response():
     mensaje = request.form['user_message']
+    email_usuario = session['idgenuine']
 
     if mensaje.startswith('word:'):
         # Solicitar un trabajo en Word
@@ -542,26 +541,16 @@ def get_response():
         return redirect(url_for('work_pdf', content=content))
     else:
         # Obtener respuesta del modelo GPT-3
-        conversaciones = cargar_conversaciones()
+        conversaciones = cargar_conversaciones(email_usuario)
         response = obtener_respuesta_gpt3(mensaje, conversaciones)
-
-        # Realizar aprendizaje activo
-        aprendizaje_activo(mensaje, response, conversaciones)
+        guardar_conversaciones(email_usuario, conversaciones)
+        print(conversaciones)
+        return response
 
         # Guardar conversaciones actualizadas
-        guardar_conversaciones(conversaciones)
-
-        # Convertir la respuesta de texto a voz
-        convertir_texto_a_voz(response)
-
-        return response
 
 
 if __name__ == '__main__':
-    crear_archivos()
-
-    # Cargar datos previos
-    cambios = cargar_conversaciones()
 
     # Iniciar la aplicación Flask
-    app.run(debug=True)
+    app.run(debug=True) 
